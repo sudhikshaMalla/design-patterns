dfb9016acacfd1c904374f079318e5dd6dbb257f
Venu Boppudi <venu.boppudi@zemosolabs.com>
2022-06-01 10:05:27 +0530

    chore: clean main repo

--- src/CreationalPatterns/abstractFactory/factories/UIFactory.java
+++
@@ -1,9 +0,0 @@
-package CreationalPatterns.abstractFactory.factories;
-
-import CreationalPatterns.abstractFactory.backgrounds.Background;
-import CreationalPatterns.abstractFactory.foregrounds.Foreground;
-
-public interface UIFactory {
-    Background createBackground();
-    Foreground createForeground();
-}
